0d1d14b1e1af99dc502cc4c1594ecf548180756d
youcoward <1071189656@qq.com>
2018-08-13 00:14:49 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -115,8 +115,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/youcoward/blog-copy/tree/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/youcoward/blog-copy/tree/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/youcoward/blog-copy/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/youcoward/blog-copy/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
